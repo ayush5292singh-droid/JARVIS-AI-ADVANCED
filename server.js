aa3ee0111a4a644efed768e9fa4e15326bc71d0a
Ayush singh <ayush5292singh@gmail.com>
2026-08-29 19:22:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.OPENAI_API_KEY;
 
 const server = http.createServer(async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+    res.writeHead(204);
+    res.end();
+    return;
+}
 
     // Show JARVIS website
     if (req.method === "GET" && req.url === "/") {
